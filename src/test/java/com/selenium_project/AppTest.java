ca672a2a625c0e4fad8e4509436f29da68daafc6
unknown <201904866@bethlehem.edu>
2024-11-02 19:12:09 +0200

    first commit

---
+++ src/test/java/com/selenium_project/AppTest.java
@@ -0,0 +1,41 @@
+package com.selenium_project;
+
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+import static org.testng.Assert.*;
+
+import org.testng.Assert;
+
+/**
+ * Unit test for simple App.
+ */
+
+@Listeners(CustomTestListener.class)
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+
+    @Test
+    public void isFlase () {
+        assertFalse(false);
+    }
+
+     @DataProvider(name = "excelData")
+    public Object[][] excelDataProvider() {
+        return ExcelUtil.readExcel("src/test/resources/Book1.xlsx"); // Update the path as needed
+    }
+
+    @Test(dataProvider = "excelData")
+    public void testFromExcel(String name, String age) {
+        Assert.assertEquals(name, "Tala", "The name is not correct.");
+        // You can add further assertions if needed
+    }
+}
